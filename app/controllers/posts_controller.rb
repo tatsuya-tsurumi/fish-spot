09c3dc29e9c99bfcf30351597e4316919d580881
tatsuya-tsurumi <turumi4551@gmail.com>
2021-12-09 18:33:31 +0900

    コントローラーへnew，createアクション記述。バリデーション記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,24 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :title, :aimble_fish, :time_zone_id, :trick, :comment).merge(user_id: current_user.id ) 
+  end
+
 end
