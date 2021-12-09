89923d81be2461fd098cefa90acda1e26078020d
tatsuya-tsurumi <turumi4551@gmail.com>
2021-12-09 18:32:49 +0900

    active hash記述。（time_zone)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-
-  validates :image, presence: true
-  varidates :titile, presence: true
-  validates :aimble_fish, presence: true
-  validates :time_zone, presence: true
   
 
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :aimble_fish, presence: true
+  validates :time_zone_id, numericality: { other_than: 1 ,message: "can't be blank"} 
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :time_zone
 end
