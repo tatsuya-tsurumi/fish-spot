e787ddd210b8aff84dd1ceae35ff28037ec855c7
tatsuya-tsurumi <turumi4551@gmail.com>
2021-12-09 16:00:47 +0900

    コメント記入欄追加

--- db/migrate/20211209051803_create_posts.rb
+++ db/migrate/20211209051803_create_posts.rb
@@ -1,7 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.string :title, null: false
+      t.string :aimble_fish, null: false
+      t.integer :time_zone, null: false
+      t.string :trick
+      t.text :comment
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
