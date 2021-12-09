c5c9c0aa33c1ceeb9f44b35bcf029855055e30ae
tatsuya-tsurumi <turumi4551@gmail.com>
2021-12-09 15:58:56 +0900

    各モデルへアソシエーション記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :image, presence: true
+  varidates :titile, presence: true
+  validates :aimble_fish, presence: true
+  validates :time_zone, presence: true
+  
+
 end
