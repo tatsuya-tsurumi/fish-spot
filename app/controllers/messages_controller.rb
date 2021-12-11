1affd27817a8c26775b9da6c4cd02d5fba1226c7
tatsuya-tsurumi <turumi4551@gmail.com>
2021-12-11 18:23:54 +0900

    index、createアクション内容記述

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,11 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
     @post = Post.find(params[:post_id])
   end
 
   def create
+    @post = Post.find(params[:post_id])
+    @message = @post.messages.new#(message_params)
   end
 end
