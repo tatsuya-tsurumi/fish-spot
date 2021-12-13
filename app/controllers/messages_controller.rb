62faad48a6aba0f82080cc731137d0bd2d44eadb
tatsuya-tsurumi <turumi4551@gmail.com>
2021-12-13 15:33:18 +0900

    コントローラーへ送信時の記述

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,10 +2,21 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @post = Post.find(params[:post_id])
+    @messages = @post.messages.includes(:user)
   end
 
   def create
     @post = Post.find(params[:post_id])
-    @message = @post.messages.new#(message_params)
+    @message = @post.messages.new(message_params)
+    if @message.save
+      redirect_to post_messages_path(@post)
+    else
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:comment).merge(user_id: current_user.id)
   end
 end
